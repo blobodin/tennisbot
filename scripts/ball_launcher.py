d9db9b659189cad6a9bce19cb523189c86c0dba8
Beau Lobodin <blobodin@caltech.edu>
2021-11-28 11:13:12 +0000

    Added spawn and delete functions for tennisball, tested in gazebo

--- scripts/ball_launcher.py
+++ scripts/ball_launcher.py
@@ -1,10 +1,28 @@
 # coding=utf-8
 import rospy
 import rospkg
-from gazebo_msgs.srv import GetModelState
+from gazebo_msgs.srv import GetModelState, GetLinkState, SetModelState, DeleteModel, SpawnModel
 from gazebo_msgs.msg import ModelState
-from gazebo_msgs.srv import SetModelState
+from geometry_msgs.msg import *
 
+def delete_ball():
+    # Deleting a model
+    rospy.ServiceProxy('gazebo/delete_model', DeleteModel)("tennisball")
+
+def spawn_ball(init_pos):
+    # Adding a model
+    spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+    with open("/home/beau/Desktop/Caltech/Senior/Fall/me133a/133ws/src/tennisbot/models/tennisball/model.sdf", "r") as f:
+        product_xml = f.read()
+
+    item_name = "tennisball"
+
+    init_pose = Pose()
+    init_pose.position.x = init_pos[0]
+    init_pose.position.y = init_pos[1]
+    init_pose.position.z = init_pos[2]
+
+    spawn_model(item_name, product_xml, "", init_pose, "world")
 
 def launch_ball(init_pos, init_vel, model_name):
     state_msg = ModelState()
@@ -37,5 +55,5 @@ def launch_ball(init_pos, init_vel, model_name):
           '/gazebo/set_model_state', SetModelState)
         resp = set_state(state_msg)
         print(state_msg)
-    except rospy.ServiceException, e:
-        print "Service call failed: %s" % e]
+    except rospy.ServiceException:
+        print("Service call failed")
